7b56938d35e4980b06ff2024d6a8db09e8d328b3
Adam Krizar <48439304+ApocalypseArisen@users.noreplay.github.com>
2025-02-21 19:46:35 +0100

    Create website main bar

--- simple-music_database/src/app/app.component.ts
+++ simple-music_database/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'simple-music_database';
+  title = 'AiMusicBot database';
 }
